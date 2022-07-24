a5cd0cb3a5cae7f3544127dcce68f3d7a5dd5d83
folsea <folsea1@gmail.com>
2022-07-24 10:15:36 -0500

    created basic java scrpt

---
+++ JS/main.js
@@ -0,0 +1,3 @@
+var A="This is the begining of the srting", B= "and this is the end of the string ";//this is the variables
+window.alert(A + B );//this is the alert
+document.write( A+B);//this is the document 
